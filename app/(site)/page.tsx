1a4ffe8955d0ef23d3f45e32ab770bd6bf2470bf
Leonardo Dillan <ldbraz@stefanini.com>
2023-08-03 00:13:22 -0300

    Fixed bugs

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           height="48"
           width="48"
           className="mx-auto w-auto"
-          src="/images/logo.png"
+          src="/images/chat-logo.png"
         />
         <h2
           className='
